4f2d288dfaac9c849a403d42c19cdbb15c7dd0b4
ndkells <38786744+ndkells@users.noreply.github.com>
2024-06-18 17:29:09 +0200

    Jump & Faces

--- ActionCubes/Assets/Scripts/Player.cs
+++ ActionCubes/Assets/Scripts/Player.cs
@@ -4,6 +4,11 @@ using UnityEngine.SceneManagement;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float speed = 5;
+    [SerializeField] private float jumpForce = 5;
+    private bool isGrounded;
+
+
+
     private Rigidbody rb;
 
     private void Awake()
@@ -14,6 +19,13 @@ public class Player : MonoBehaviour
     private void Update()
     {   
         rb.velocity = new Vector2(Input.GetAxis("Horizontal") * 5, rb.velocity.y);
+
+        // Sprungmechanik
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;  // Aktualisiere den Bodenkontakt-Status
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -23,6 +35,11 @@ public class Player : MonoBehaviour
             rb.constraints = RigidbodyConstraints.None;
 
             Invoke(nameof(Restart), 2);
+        }
+
+        if (collision.gameObject.tag == "Ground")  // Stelle sicher, dass der "Ground" Tag in Unity gesetzt ist
+        {
+            isGrounded = true;
         }
     }
 
